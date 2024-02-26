8972272e48d1379eaf14d63bcc3abba32e37bbb2
Oksana Kormilina <keysb@mail.ru>
2024-02-26 13:39:35 +0400

    Add imperative animation by submitting empty form

--- src/components/NewChallenge.jsx
+++ src/components/NewChallenge.jsx
@@ -1,15 +1,16 @@
-import { useContext, useRef, useState } from 'react';
-import {motion} from 'framer-motion'
+import { useContext, useRef, useState } from "react";
+import { motion, useAnimate, stagger } from "framer-motion";
 
-
-import { ChallengesContext } from '../store/challenges-context.jsx';
-import Modal from './Modal.jsx';
-import images from '../assets/images.js';
+import { ChallengesContext } from "../store/challenges-context.jsx";
+import Modal from "./Modal.jsx";
+import images from "../assets/images.js";
 
 export default function NewChallenge({ onDone }) {
   const title = useRef();
   const description = useRef();
   const deadline = useRef();
+
+  const [scope, animate] = useAnimate(); //names up to you
 
   const [selectedImage, setSelectedImage] = useState(null);
   const { addChallenge } = useContext(ChallengesContext);
@@ -33,6 +34,11 @@ export default function NewChallenge({ onDone }) {
       !challenge.deadline.trim() ||
       !challenge.image
     ) {
+      animate(
+        "input, textarea",
+        {rotate: [2, 0, -2, 0, ] , borderColor: ["#ff0000", '#d9e2f1']},
+        { type: "spring", duration: 0.2, delay: stagger(0.05)}
+      ); // css pointer - element or .class, then what to animate, then configuration obj
       return;
     }
 
@@ -42,7 +48,7 @@ export default function NewChallenge({ onDone }) {
 
   return (
     <Modal title="New Challenge" onClose={onDone}>
-      <form id="new-challenge" onSubmit={handleSubmit}>
+      <form id="new-challenge" onSubmit={handleSubmit} ref={scope}>
         <p>
           <label htmlFor="title">Title</label>
           <input ref={title} type="text" name="title" id="title" />
@@ -58,20 +64,23 @@ export default function NewChallenge({ onDone }) {
           <input ref={deadline} type="date" name="deadline" id="deadline" />
         </p>
 
-        <motion.ul variants={{
-          visible: {transition:{staggerChildren: 0.1} }}} //set delay between children li
-        id="new-challenge-images">
+        <motion.ul
+          variants={{
+            visible: { transition: { staggerChildren: 0.1 } },
+          }} //set delay between children li
+          id="new-challenge-images"
+        >
           {images.map((image) => (
             <motion.li
-            variants={{
-              hidden:{opacity: 0, scale: 0.5},
-              visible: {opacity: 1, scale: [0.8, 1.3, 1]} //adding [] constructs keyframes, or steps of animation
-            }}
-            exit={{opacity: 1, scale: 1}}
-            transition={{type: 'spring', duration: 1}}
+              variants={{
+                hidden: { opacity: 0, scale: 0.5 },
+                visible: { opacity: 1, scale: [0.8, 1.3, 1] }, //adding [] constructs keyframes, or steps of animation
+              }}
+              exit={{ opacity: 1, scale: 1 }}
+              transition={{ type: "spring", duration: 1 }}
               key={image.alt}
               onClick={() => handleSelectImage(image)}
-              className={selectedImage === image ? 'selected' : undefined}
+              className={selectedImage === image ? "selected" : undefined}
             >
               <img {...image} />
             </motion.li>
